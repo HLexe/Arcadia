2c0ab83659ef488adff2828422bad5e09744fcbc
Hector León <147671537+HLexe@users.noreply.github.com>
2024-01-29 20:27:20 -0300

    algoritmo basico condicional/ciclo JS

---
+++ js/main.js
@@ -0,0 +1,26 @@
+//ALGORITMO CONDICIONAL MUUUUUUY BASICO
+// alert('Bienvenido a Arcadia.');
+
+// let nombre = prompt('Cual es tu nombre?');
+// let apellido = prompt('cual ess tu apellido?');
+
+// let nombreCompleto = nombre + ' ' + apellido;
+//     alert('Hola ' + nombreCompleto);
+
+// let edad = prompt('Por favor ingresa tu edad.');
+
+// if(edad >=18){
+//    alert('Puedes ingresar al sitio, bienvenido.');
+// }else{
+//     alert('Debes ser mayor de edad para ingresar al sitio.');
+// }
+
+//ALGORITMO CICLO MUUUUUUY BASICO
+
+for (let xp = 1; xp < 11; xp+=1){
+    if (xp === 11) {
+        break;
+    }
+    console.log('XP ' + xp);
+}
+console.log('LVL UP!!!')
