46feb11d63e59d79fba4f35336ad1310ff302e67
Hector León <147671537+HLexe@users.noreply.github.com>
2024-03-04 20:28:17 -0300

    create cart page, add js stuff, make a complete mess

--- js/main.js
+++ js/main.js
@@ -1,179 +1,107 @@
-//ALGORITMO CONDICIONAL MUUUUUUY BASICO
-// alert('Bienvenido a Arcadia.');
-
-// let nombre = prompt('Cual es tu nombre?');
-// let apellido = prompt('cual ess tu apellido?');
-
-// let nombreCompleto = nombre + ' ' + apellido;
-//     alert('Hola ' + nombreCompleto);
-
-// let edad = prompt('Por favor ingresa tu edad.');
-
-// if(edad >=18){
-//    alert('Puedes ingresar al sitio, bienvenido.');
-// }else{
-//     alert('Debes ser mayor de edad para ingresar al sitio.');
-// }
-
-//ALGORITMO CICLO MUUUUUUY BASICO
-
-// for (let xp = 1; xp < 11; xp+=1){
-//     if (xp === 11) {
-//         break;
-//     }
-//     console.log('XP ' + xp);
-// }
-// console.log('LVL UP!!!')
-
-// alert('Por favor identifiquese.');
-
-// let nombre = prompt('Ingrese Nombre');
-// let apellido = prompt('Ingrese Apellido');
-
-// alert('Por favor ingrese numero de ID y PIN');
-
-// function solicitarNumeroId(){
-//     let numeroId = prompt('Ingrese ID');
-//     let pin = prompt('Ingrese PIN');
-
-//     if(numeroId ==='1234' && pin ==='4321'){
-//         alert('Bienvenido ' + nombre + ' ' + apellido + '.');
-//     }else if(numeroId !='' && pin !=''){
-//         alert('Informacion incorrecta, intente nuevamente.');
-//     }
-// }
-// solicitarNumeroId()
-
-//REGISTRO
-alert("Bienvenido a Arcadia.\nPor favor regístrate para comprar.");
-
-let username = "";
-let password = "";
-
-while (username === ""){
-    username = prompt("Ingresa nombre de usuario: ");
-}
-
-while (password === "" || password.length < 4 || password.length > 10){
-    password = prompt("Ingresa password (entre 4 y 10 caracteres): ");
-}
-
-alert("Registro exitoso.\nBienvenido a Arcadia, " + username + ".");
 
 
-let items = [
-    {
-        name: "Raccoon",
-        price: 8000
-    },
-    {
-        name: "Monke",
-        price: 8000
-    },
-    {
-        name: "Cat",
-        price: 8000
-    },
-    {
-        name: "Trooper",
-        price: 8000
-    },
-    {
-        name: "Deathstar",
-        price: 8000
-    },
-    {
-        name: "Daft",
-        price: 8000
-    },
-    {
-        name: "Car",
-        price: 8000
-    },
-    {
-        name: "Retro",
-        price: 8000
-    },
-    {
-        name: "Pacman",
-        price: 10000
-    },
-    {
-        name: "Cyberpunk",
-        price: 10000
-    },
-];
+//ITEMS ARRAY
 
-function showitems() {
-    console.log("Listado de productos");
-    for (let i = 0; i < items.length; i++) {
-    console.log(`${i + 1}. ${items[i].name} - CLP ${items[i].price}`);
-        }
-}
+const ITEM =[
 
-    function buyitems() {
-        let cart = [];
-        let option;
-    
-        while (option !== "0") {
-        showitems();
-        console.log("0. Finalizar Compra");
-        option = prompt("Selecciona un número (del 1 al 10) para comprar, 0 para terminar.\nPuedes ver la lista en la consola.");
-    
-        if (option !== "0") {
-            let itemIndex = Number(option) - 1;
-    
-            if (itemIndex >= 0 && itemIndex < items.length) {
-            let quantity = Number(prompt(`Cuantas ${items[itemIndex].name} quieres comprar?`));
-            let item = {
-                ...items[itemIndex],
-                quantity: quantity
-            };
-            cart.push(item);
-            alert(`Has añadido al carrito: ${item.name}.`);
-            console.log(`Has añadido al carrito: ${item.name}.`);
-    
-            alert(`Añadiste ${item.quantity} t-shirts al carrito.`);
-            console.log(`Añadiste ${item.quantity} t-shirts al carrito.`);
-            } else {
-            alert("Opcion invalida. Intenta nuevamente.")
-            }
-        }
-        }
+    {id: 1, name: "raccoon", image: "../images/", price: 8.000 },
+    {id: 2, name: "monke", image: "../images/", price: 8.000 },
+    {id: 3, name: "cat", image: "../images/", price: 8.000 },
+    {id: 4, name: "trooper", image: "../images/", price: 8.000 },
+    {id: 5, name: "deathstar", image: "../images/", price: 8.000 },
+    {id: 6, name: "daft", image: "../images/", price: 8.000 },
+    {id: 7, name: "car", image: "../images/", price: 8.000 },
+    {id: 8, name: "retro", image: "../images/", price: 8.000 },
+    {id: 9, name: "pacman", image: "../images/", price: 10.000 },
+    {id: 10, name: "cyberpunk", image: "../images/", price: 10.000 },
+    {id: 11, name: "netrunner", image: "../images/", price: 10.000 },
+    {id: 12, name: "synthcity", image: "../images/", price: 10.000 },
+    {id: 13, name: "arasaka", image: "../images/", price: 10.000 },
+    {id: 14, name: "bladerunner", image: "../images/", price: 10.000 },
+    {id: 15, name: "bladerunner2", image: "../images/", price: 10.000 },
+    {id: 16, name: "cyberpunk2", image: "../images/", price: 10.000 },
+    {id: 17, name: "cybercat", image: "../images/", price: 10.000 },
+    {id: 18, name: "cyberpunkGirl", image: "../images/", price: 10.000 },
+    {id: 19, name: "cyberpunkGirl2", image: "../images/", price: 10.000 },
+    {id: 20, name: "samurai", image: "../images/", price: 10.000 },
+    {id: 21, name: "maelstrom", image: "../images/", price: 10.000 },
+    {id: 22, name: "samurai2", image: "../images/", price: 10.000 },
+    {id: 23, name: "traumaTeam", image: "../images/", price: 10.000 },
+    {id: 24, name: "tron", image: "../images/", price: 10.000 }
 
-        
-        console.log("Resumen de compra.");
-        let total = 0;
+]
 
-        for (let i = 0; i < cart.length; i++) {
-        let items = cart[i];
-        total += items.price * items.quantity;
-        }
-   
-        alert(`Total a pagar: CLP ${total.toFixed(2)}`);
-        console.log(`Total de tu compra: CLP ${total.toFixed(2)}`);
+let cartItemsQuantity = 0;
+
+let cart = JSON.parse(localStorage.getItem('cart') )|| [];
+
+//COUNTER
+
+function updateCartIcon(){
+    const CART_ICON = document.getElementById('cartItemsQuantity');
+    if(CART_ICON){
+        CART_ICON.textContent = cartItemsQuantity;
     }
-
-    buyitems();
-
-
-while(true){
-    let color = prompt("Selecciona un color (Blanco/Negro)").toString().toUpperCase();
-    let talla = prompt("Selecciona tu talla (S, M, L, XL)").toString().toUpperCase();
-
-    switch (color) {
-        case "NEGRO":
-            alert(`Elegiste ${color}, talla ${talla}`);
-            break;
-        case "BLANCO":
-            alert(`Seleccionaste ${color}, talla ${talla}`);
-            break;
-        default:
-            alert("Ingresa un color valido.");
-        continue;
-    }
-    break;
 }
 
-alert("Compra finalizada, en breve recibiras un email con los detalles.")
-alert("Gracias por tu compra.")
+//SHOW CART ITEMS
+
+function showCartItems(){
+    const CART_ITEMS = document.getElementById('cart-items');
+    CART_ITEMS.innerHTML = '';
+
+    cart.forEach(ITEMS => {
+        const ITEM= document.createElement('div');
+        item.textContent = ITEMS.nombre;
+    });
+   CART_ITEMS.appendChild(ITEMS);
+}
+
+//CARDS
+function createItemCard(){
+    const CONTAINER = document.getElementById('item-container');
+
+
+    ITEM.forEach(ITEM =>{
+        const CARD = document.createElement('div');
+        CARD.classList.add('item-card');
+
+
+        const imagen = document.createElement('img');
+        image.src = ITEM.image;
+        image.alt = ITEM.nombre;
+        image.classList.add('item-image');
+        card.appendChild(image);
+        const NAME = document.createElement('h3');
+        NAME.textContent = ITEM.name;
+        NAME.classList.add('item-name');
+        CARD.appendChild(NAME);
+
+        const PRICE = document.createElement('p');
+        prrice.textContent = ITEM.price;
+        price.classList.add('item-price');
+        card.appendChild(price);
+
+        const buyBtn = document.createElement('button');
+        buyBtn.textContent = 'Agregar Item';
+        buyBtn.classList.add('buy-button');
+        buyBtn.addEventListener('click',()=>{
+            cart.push(ITEM); //agregamos el producto al carrito :D
+            localStorage.setItem('cart', JSON.stringify(cart));
+            cartItemsQuantity++;
+            updateCartIcon();
+            showCartItems();
+        })
+
+        .appendChild(buyBtn)
+
+        CONTAINER.appendChild(card);
+
+    })
+}
+
+createItemCard();
+
+updateCartIcon();
+
+showCartItems();
